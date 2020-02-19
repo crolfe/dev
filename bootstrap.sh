5e7660b56c75696b9d859feafb2f6639edcf854a
Colin Rolfe <crolfe@gmail.com>
2020-02-19 11:17:15 -0800

    install python3-pip and wheel

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,10 @@
 set -e
 
 if [[ ! -d venv ]]; then
-	sudo apt-get install -y python3-venv
+	sudo apt-get install -y python3-venv python3-pip
 	python3 -m venv venv
 fi
 
-./venv/bin/pip install -U ansible ansible-lint
+./venv/bin/pip install -U ansible ansible-lint wheel
+
+sudo apt-get update
